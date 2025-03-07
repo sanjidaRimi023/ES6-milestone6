6bacd3f22db761c8229fadaae32776f52df908fd
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-03-07 12:50:36 +0600

    complete modules class

---
+++ modeul30/module31/js/inheritance.js
@@ -0,0 +1,29 @@
+// class students {
+//     constructor(semester, shift, name) {
+//         this.semester = semester;
+//         this.shift = shift;
+//         this.name = name;
+//     }
+//     introduce() {
+//         console.log(`The 1st boy name is ${this.name} . Her semester name ${this.semester} . shift name ${this.shift}.`);
+//     }
+// }
+
+// class student extends students {
+//     constructor(semester, shift, name, department) {
+//         super(semester, shift, name);
+//         this.department = department;
+//     }
+//     about() {
+//         console.log(`${this.name} is good student from ${this.department} department`);
+//     }
+
+// }
+// const iran = new student('5th', '2nd', 'rihan', 'CST');
+// iran.introduce();
+// iran.about();
+// console.log(Object.getPrototypeOf(iran));
+
+// const topper = new student('5th', '2nd', 'binoy', 'CSE');
+// topper.introduce()
+// topper.about()
---
+++ modeul30/module31/js/this.js
@@ -0,0 +1,14 @@
+// class boys {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     detail() {
+//         console.log(`the person name is ${this.name}. his age is ${this.age}`);
+
+//     }
+// }
+// const sharafat = new boys('binoy', 20);
+// sharafat.detail()
+
+
