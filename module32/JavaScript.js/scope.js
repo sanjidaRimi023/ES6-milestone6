2b12b33a104fec97ee12398b973fec28d88f3abf
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-03-09 00:19:48 +0600

    closure

--- module32/JavaScript.js/scope.js
+++ module32/JavaScript.js/scope.js
@@ -1,17 +1,18 @@
-//let and const block scope
-function sum(num1, num2) {
-    const result = num1 - num2;
-    console.log(result);
-}
-sum(12, 5);
-// {} bracket er vitor ja lekha hobe ta bahire access kora jbe na ai ta holo block scope
+// //let and const block scope
+// function sum(num1, num2) {
+//     const result = num1 - num2;
+//     console.log(result);
+// }
+// sum(12, 5);
+// // {} bracket er vitor ja lekha hobe ta bahire access kora jbe na ai ta holo block scope
 
-// var global scope
+// // var global scope
 
-function multi(num3, num4) {
-    // console.log(result2); output undefined
-    var result2 = num3 * num4;
-    console.log(result2);
-}
+// function multi(num3, num4) {
+//     // console.log(result2); output undefined
+//     var result2 = num3 * num4;
+//     console.log(result2);
+// }
 
-multi(2, 4);
+// multi(2, 4);
+// // var ke je kno jaygay theke access kora jay
