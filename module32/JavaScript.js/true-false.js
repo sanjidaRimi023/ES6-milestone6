6517d48f60480942ade73a437d0cdf017ceed7ba
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-03-08 22:15:09 +0600

    comparision === or ==

--- module32/JavaScript.js/true-false.js
+++ module32/JavaScript.js/true-false.js
@@ -1,86 +1,87 @@
 
-const name = "mahir"; //string always true hoy
-if (name) {
-    console.log('this is true');
-} else {
-    console.log('this is false');
-}
+// //string always true hoy
+// const name = "mahir";
+// if (name) {
+//     console.log('this is true');
+// } else {
+//     console.log('this is false');
+// }
 
+// //empty string false hoy
+// const fatherName = "";
+// if (fatherName) {
+//     console.log('this is true');
+// } else {
+//     console.log('this is false');
+// }
 
-const fatherName = ""; //empty string false hoy
-if (fatherName) {
-    console.log('this is true');
-} else {
-    console.log('this is false');
-}
+// // its true cz space into string
+// const matherName = " ";
+// if (matherName) {
+//     console.log('this is true');
+// } else {
+//     console.log('this is false');
+// }
 
+// //number is truthy value.(positive and negative also)
+// const num = 1;
 
-const matherName = " "; // its true cz space into string
-if (matherName) {
-    console.log('this is true');
-} else {
-    console.log('this is false');
-}
+// if (num) {
+//     console.log('this is true');
+// } else {
+//     console.log('this is false');
+// }
+// //every number is true but 0 and -0 are falsy value
+// const num2 = 0;
 
+// if (num2) {
+//     console.log('this is true');
+// } else {
+//     console.log('this is false');
+// }
+// // object truthy value and empty object also truthy value
+// const students = { name: 'rimi' };
 
-const num = 1; //number is truthy value.(positive and negative also) 
+// if (students) {
+//     console.log('this is true');
+// } else {
+//     console.log('this is false');
+// }
 
-if (num) {
-    console.log('this is true');
-} else {
-    console.log('this is false');
-}
+// //array also truthy value
+// let mark = [12, 334, 45, 656];
+// if (mark) {
+//     console.log('this is true');
+// } else {
+//     console.log('this is false');
+// }
+// // empty array also truthy value
+// let totalArr = [];
+// if (totalArr) {
+//     console.log('this is true');
+// } else {
+//     console.log('this is false');
+// }
+// // null, undefined, NaN are falsy value
 
-const num2 = 0; //every number is true but 0 and -0 are falsy value 
+// let Null = null;
+// if (Null) {
+//     console.log('this is true');
+// } else {
+//     console.log('this is false');
+// }
 
-if (num2) {
-    console.log('this is true');
-} else {
-    console.log('this is false');
-}
+// let Undefined = undefined;
+// if (Undefined) {
+//     console.log('this is true');
+// } else {
+//     console.log('this is false');
+// }
 
-const students = { name: 'rimi' };// object truthy value and empty object also truthy value
-
-if (students) {
-    console.log('this is true');
-} else {
-    console.log('this is false');
-}
-
-//array also truthy value
-let mark = [12, 334, 45, 656];
-if (mark) {
-    console.log('this is true');
-} else {
-    console.log('this is false');
-}
-// empty array also truthy value
-let totalArr = [];
-if (totalArr) {
-    console.log('this is true');
-} else {
-    console.log('this is false');
-}
-// null, undefined, NaN are falsy value
-
-let Null = null;
-if (Null) {
-    console.log('this is true');
-} else {
-    console.log('this is false');
-}
-
-let Undefined = undefined;
-if (Undefined) {
-    console.log('this is true');
-} else {
-    console.log('this is false');
-}
-
-const NAN = NaN;
-if (NAN) {
-    console.log('this is true');
-} else {
-    console.log('this is false');
-}
+// const NAN = NaN;
+// if (NAN) {
+//     console.log('this is true');
+// } else {
+//     console.log('this is false');
+// }
 
