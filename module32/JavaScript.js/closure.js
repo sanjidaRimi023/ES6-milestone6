7868b158bc8d25394167a66010756159a86b5c20
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-03-09 07:35:54 +0600

    milestone complete

--- module32/JavaScript.js/closure.js
+++ module32/JavaScript.js/closure.js
@@ -1,26 +1,26 @@
-/* closure is when a function is able to remember ans access it's lexical scope even when that function executing outside it's lexical scope
-closure access two ways :
+// /* closure is when a function is able to remember ans access it's lexical scope even when that function executing outside it's lexical scope
+// closure access two ways :
 
-1.able to remember ans access it's lexical scope
-2.when that function executing outside it's lexical scope
-*/
+// 1.able to remember ans access it's lexical scope
+// 2.when that function executing outside it's lexical scope
+// */
 
-//function text() {
+// //function text() {
+// //     let msg = "we are learning what is closure";
+// //     function sayMsg() {
+// //         console.log(msg);
+// //     }
+// //     sayMsg();
+// // }
+// // text();
+
+// function text() {
 //     let msg = "we are learning what is closure";
-//     function sayMsg() {
+//     return function () {
 //         console.log(msg);
 //     }
-//     sayMsg();
 // }
-// text();
+// let sayMsg = text();
+// console.log(sayMsg); //we call block scope function is global scope that why they return a function(14 line)
 
-function text() {
-    let msg = "we are learning what is closure";
-    return function () {
-        console.log(msg);
-    }
-}
-let sayMsg = text();
-console.log(sayMsg); //we call block scope function is global scope that why they return a function(14 line)
-
-sayMsg() //if we call global scope so they return "msg" (line15)
+// sayMsg() //if we call global scope so they return "msg" (line15)
